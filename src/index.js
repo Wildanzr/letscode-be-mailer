8c5d1726e7a9827777b7d865e7de6b3411b1a273
wildanzr <graita.sukma@gmail.com>
2023-02-07 13:37:20 +0700

    feat: dockerization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 // Config environment
 require('dotenv').config()
 
-// Mail Service
-const { MailService } = require('./mails')
-const mailService = new MailService()
+// Wait for RabbitMQ to start before starting the consumer, set timeout for 30 seconds
+setTimeout(() => {
+  // Mail Service
+  const { MailService } = require('./mails')
+  const mailService = new MailService()
 
-// Consumer
-const { Consumer } = require('./services/consumer')
-const consumer = new Consumer(mailService)
+  // Consumer
+  const { Consumer } = require('./services/consumer')
+  const consumer = new Consumer(mailService)
 
-// Start the consumer
-consumer.consumeMessage()
+  // Start the consumer
+  consumer.consumeMessage()
+}, 30000)
+
+console.log('Waiting for RabbitMQ to start...')
